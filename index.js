c2f15d269cdd39b3a0f0441090a75405f672dfa6
shivamgupta <shivam@soroco.com>
2020-08-22 16:55:09 +0530

    first working commit!

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
+const { v4: uuidv4 } = require('uuid');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+app = express();
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+
+  socket.nickname = uuidv4();
+  console.log(`${socket.nickname} connected!`);
+
+  socket.on('pause', (msg) => {
+    console.log(`${socket.nickname} paused video at ${msg}`);
+    socket.broadcast.emit('pause', msg);
+  });
+
+  socket.on('play', (msg) => {
+    console.log(`${socket.nickname} played video at ${msg}`);
+    socket.broadcast.emit('play', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`${socket.nickname} disconnected!`);
+  });
+});
+
+
+http.listen(PORT, () => {
+  console.log(`listening on *:${ PORT }`);
+});
